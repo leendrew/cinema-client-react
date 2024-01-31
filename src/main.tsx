5a376e82ca371a1613caf1eacc676d67d30bac32
leendrew <leendrew99@gmail.com>
2024-01-31 20:59:17 +0600

    chore: clear main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
+import { muiConfig } from './config';
+import { router } from '@/router';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={muiConfig}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
 );
