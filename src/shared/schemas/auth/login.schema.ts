e549becace9cef220ff7abaa2c5e12fcbb71ea99
leendrew <leendrew99@gmail.com>
2024-02-01 20:35:57 +0600

    feat: auth schema validation; feat: serializer for dto

--- src/shared/schemas/auth/login.schema.ts
+++ src/shared/schemas/auth/login.schema.ts
@@ -1,10 +1,23 @@
 import * as z from 'zod';
+import { ERROR_MESSAGE, phoneSchema } from '../shared.schema';
 
-export const phoneSchema = z.object({});
+export const MAX_PHONE_LEN = 11;
 
-export const otpSchema = z.object({});
+export const MAX_OTP_LEN = 6;
+
+const ERROR_MESSAGE_OTP_LEN = `${ERROR_MESSAGE.code} ${MAX_OTP_LEN} цифр`;
+
+export const codeSchema = z
+  .string({ required_error: ERROR_MESSAGE.required })
+  .min(MAX_OTP_LEN, ERROR_MESSAGE_OTP_LEN);
+
+export const getOtpSchema = z.object({
+  phone: phoneSchema,
+});
 
 export const loginSchema = z.object({
   phone: phoneSchema,
-  otp: otpSchema,
+  code: codeSchema,
 });
+
+export type LoginSchema = z.infer<typeof loginSchema>;
--- src/shared/schemas/shared.schema.ts
+++ src/shared/schemas/shared.schema.ts
@@ -1,22 +1,53 @@
 import * as z from 'zod';
 
-export const ERROR_MESSAGE_REQUIRED = 'Поле является обязательным';
-export const ERROR_MESSAGE_CODE = 'Код должен содержать';
-export const ERROR_MESSAGE_COMBINATION_ALPHABET =
-  'Значение должно быть задано с использованием одного из следующих алфавитов: кириллического, латинского';
-export const ERROR_MESSAGE_DIFFERENT_ALPHABET = 'Значения заданы с использованием разных алфавитов';
-export const ERROR_MESSAGE_INCORRECT_FORMAT = 'Некорректный формат';
+export const ERROR_MESSAGE = {
+  api: 'Произошла ошибка',
+  required: 'Поле является обязательным',
+  code: 'Код должен содержать',
+  combinationAlphabet:
+    'Значение должно быть задано с использованием одного из следующих алфавитов: кириллического, латинского',
+  differentAlphabet: 'Значения заданы с использованием разных алфавитов',
+  incorrect: 'Поле заполнено некорректно',
+  incorrectFormat: 'Некорректный формат',
+  maxNameLen: 'Поле не может быть больше',
+};
+
+export const ERROR_MESSAGE_API = 'Произошла ошибка';
 
 export const MAX_NAME_LEN = 60;
 
-// export const login = z
-//   .string({
-//     required_error: ERROR_MESSAGE_REQUIRED,
-//   })
-//   .min(MIN_LENGTH_LOGIN, `${ERROR_MESSAGE_SHORT} ${MIN_LENGTH_LOGIN}`);
+export const phoneSchema = z
+  .string({ required_error: ERROR_MESSAGE.required })
+  .refine(
+    (value) => /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/.test(value),
+    ERROR_MESSAGE.incorrect,
+  );
 
-// export const password = z
-//   .string({
-//     required_error: ERROR_MESSAGE_REQUIRED,
-//   })
-//   .min(MIN_LENGTH_PASSWORD, `${ERROR_MESSAGE_SHORT} ${MIN_LENGTH_PASSWORD}`);
+const serializeDataRules = {
+  /**
+   * '+7 999 888 77 66' -> '79998887766'
+   */
+  phone: (value: string) => value.replace(/[^\d.-]+/g, ''),
+  /**
+   * '123456' -> 123456
+   */
+  code: (value: string) => parseInt(value, 10),
+};
+
+// TODO: refactor typing
+export const serializeData = (data: object) =>
+  Object.entries(data).reduce((acc, [key, value]) => {
+    // @ts-expect-error object keys
+    const serializeFn = serializeDataRules[key];
+    if (!serializeFn) {
+      // @ts-expect-error object keys
+      acc[key] = value;
+
+      return acc;
+    }
+
+    // @ts-expect-error object keys
+    acc[key] = serializeFn(value);
+
+    return acc;
+  }, {});
