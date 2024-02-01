e2613393b4a6276e604034dab76827991915c7bd
leendrew <leendrew99@gmail.com>
2024-02-01 20:33:47 +0600

    feat: api config

---
+++ src/config/api.config.ts
@@ -0,0 +1,27 @@
+import Axios from 'axios';
+import { envConfig } from './env.config';
+import { authStore } from '@/store';
+
+export const axios = Axios.create({
+  baseURL: envConfig.apiUrl,
+});
+
+axios.interceptors.request.use((config) => {
+  const authState = authStore.getState();
+  if (authState.token) {
+    config.headers.Authorization = `Bearer ${authState.token}`;
+  }
+
+  return config;
+});
+
+// TODO: unauthorized redirect to login
+
+export interface ApiResponseSuccess {
+  success: true;
+}
+
+export interface ApiResponseFail {
+  success: false;
+  reason: string;
+}
